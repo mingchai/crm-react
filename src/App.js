bb792b80e4ca9d3022dca930220573ea41028bcf
Ming Chai <ming.chai4@gmail.com>
2019-04-15 16:13:38 -0700

    add react-router-dom components to separate pages

--- src/App.js
+++ src/App.js
@@ -2,20 +2,56 @@ import React, { Component } from 'react';
 import ClientIndex from './components/ClientIndexPage';
 import './App.css';
 import SignInPage from './components/SignInPage';
+import HomePage from "./components/HomePage";
+import {User, Session} from './requests';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import AuthRoute from "../src/components/AuthRoute";
 
 class App extends Component {
-  render() {
-    return (
-      <>
-      <div className="App">
-        <header className="App-header">
-          <h1>New App</h1>
-        </header>
-      </div>
+  constructor(props){
+    super(props);
+    this.state = {currentUser:null};
 
-      <ClientIndex/>
-      <SignInPage/>
-      </>
+    this.getCurrentUser = this.getCurrentUser.bind(this);
+    this.signOut = this.signOut.bind(this);
+  }
+
+  getCurrentUser(){
+    User.current().then(data => {
+      const {current_user: currentUser} = data;
+      if(currentUser){this.setState({currentUser})}
+    })
+  }
+
+  componentDidMount(){
+    this.getCurrentUser();
+  }
+
+  signOut(e){
+    e.preventDefault();
+    this.setState({currentUser:null});
+    Session.destroy();
+  }
+
+  render() {
+    const {currentUser} = this.state;
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <h1>New App</h1>
+          </header>
+          <a href="#not-used" onClick = {this.signOut}>Logout</a>
+          <Switch>
+            <AuthRoute isAuth={currentUser} path="/" exact component={HomePage}/>
+            <Route path="/sign_in" exact render = {routeProps => <SignInPage {...routeProps} onSignIn = {this.getCurrentUser}/>}>
+            </Route>
+            <AuthRoute isAuth = {currentUser} exact path="/clients" component={ClientIndex}/>
+            <ClientIndex/>
+          </Switch>
+        </div>
+
+      </BrowserRouter>
     );
   }
 }
