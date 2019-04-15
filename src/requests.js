44ff745c79761577f2ff47b6dadd397c4d758d17
Ming Chai <ming.chai4@gmail.com>
2019-04-14 20:53:59 -0700

    add requests for session#create and session#destroy

--- src/requests.js
+++ src/requests.js
@@ -1,7 +1,28 @@
-const BASEURL = `http://localhost:3000/api/v1/`;
+const BASE_URL = `http://localhost:3000/api/v1`;
 
 export const Clients = {
     all(){
-        return fetch(`${BASEURL}/clients`, {credentials:'include'}).then(res => res.json)
+        return fetch(`${BASE_URL}/clients`,{
+            credentials:"include"
+        }).then(res => res.json())
     }
 };
+
+export const Session = {
+    create(params) {
+      return fetch(`${BASE_URL}/session`, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(params)
+      }).then(res => res.json());
+    },
+    destroy() {
+      return fetch(`${BASE_URL}/session`, {
+        method: "DELETE",
+        credentials: "include"
+      }).then(res => res.json());
+    }
+  };
