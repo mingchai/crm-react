1661bacc6f599a24f2d05726eafaed809bfb7943
Ming Chai <ming.chai4@gmail.com>
2019-04-15 16:18:12 -0700

    add homepage component to emulate Rails app

---
+++ src/components/HomePage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import background from '../images/chairs.jpg';
+export default function HomePage() {
+  return (
+    <div>
+      <img src={background} alt=""/>
+    </div>
+  )
+}
