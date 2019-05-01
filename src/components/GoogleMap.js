5f3a14bc3f0ac4799bfbcd2398299c4c6150e365
Ming Chai <ming.chai4@gmail.com>
2019-05-01 16:24:17 -0700

    adjust map dimensions

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -14,16 +14,17 @@ export default class GoogleMap extends Component {
 
   render(){
     const {lat, long, center, zoom} = this.props;
+    
     return (
-      <div style={{ height: '100vh', width: '100%' }}>
+      <div style={{ height: '360px', width: '100%' }}>
       {
-        lat === null || long === null ? 
+        lat === undefined || long === undefined ? 
         <h1>Client Address is not Mappable. Default Map Provided</h1> : 
         null
       }
         <GoogleMapReact
           bootstrapURLKeys={{ key: 'AIzaSyD2E3HcI677WQ3yTPNalNcYVqbPChm56cs' }}
-          defaultCenter={(lat === null) ? center : [lat, long]}
+          defaultCenter={(lat === undefined) ? center : [lat, long]}
           defaultZoom={zoom}
         />
       </div>
